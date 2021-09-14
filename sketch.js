205fc723eb369379540fe12112132a32589d24ca
OmshivaniRaj <84126951+OmshivaniRaj@users.noreply.github.com>
2021-09-14 08:59:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color("blue");
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.2, isStatic:true});
 	World.add(world, packageBody);
 	
 	box1 = createSprite(260,600,10,110);
